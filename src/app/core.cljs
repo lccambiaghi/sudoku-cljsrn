413e05539c6525dfa5ded5c7053d9db8afeb3525
Luca Cambiaghi <luca.cambiaghi@maersk.com>
2020-08-23 23:36:52 +0200

    Revert "Refactor almost working"
    This reverts commit 47ccb993610f96d4bb2115a94c2819bde30f6f67.

--- src/app/core.cljs
+++ src/app/core.cljs
@@ -39,9 +39,7 @@
                :width             40      :height            40}]
     style))
 
-(def numbers (into [] (for [n (range 1 (inc 81))] {:key n})))
-
-(defonce grid (->> (complete-grid) flatten (sort-by :index) (into [])))
+(defonce grid (->> (sudoku/complete-grid) flatten (sort-by :index) (into [])))
 
 (def state (r/atom grid))
 ;; (assoc-in @grid [1 :key] 4)
@@ -55,9 +53,9 @@
           index  (.-index (.-item item))]
       (if number
         [rrn/text {:style (:number styles)} (.-key (.-item item))]
-        [rrn/text-input {:style (:number styles)
+        [rrn/text-input {:style       (:number styles)
                          ;; :placeholder (str index)
-                         ;; :on-change-text #(swap! grid assoc-in [(dec index) :key] %)
+                         :on-change-text #(swap! grid assoc-in [(dec index) :key] %)
                          ;; :default-value "!!"
                          }]))]))
 
@@ -65,8 +63,7 @@
   [rrn/safe-area-view {:style (:container styles)}
    [rrn/text {:style (:title styles)} "Sudoku"]
    [rrn/flat-list
-    {:data        @state
-     ;; :data        numbers
+    {:data        @grid
      :render-item render-number
      :num-columns 9}]])
 
